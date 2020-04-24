7b92a6993f9326a092179a9ca8ac03188d8e9ba1
Milena Stancheva <milena.stancheva@miracl.com>
2020-04-24 11:59:05 +0300

    Fix footer

--- Models/Regions/Footer.cs
+++ Models/Regions/Footer.cs
@@ -1,13 +1,15 @@
 ﻿using Piranha.Extend;
 using Piranha.Extend.Fields;
-using System.Collections.Generic;
 
 namespace HeroesCup.Models.Regions
 {
     public class Footer
     {
         [Field]
-        public HtmlField UpperContent { get; set; }
+        public HtmlField UpperContentLeft { get; set; }
+
+        [Field]
+        public HtmlField UpperContentRight { get; set; }
 
         [Field]
         public HtmlField MiddleContent { get; set; }
