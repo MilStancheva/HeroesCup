d05f57c15a52c17155c04c4b1151409ca499389b
Andrey <adautev@gmail.com>
2020-05-21 20:42:35 +0300

    Added publish stub

---
+++ CD/setup.sh
@@ -0,0 +1,16 @@
+apt-get install mariadb-server
+apt-get install nginx
+useradd actions-runner
+passwd actions-runner
+cd /var
+mkdir actions-runner && cd actions-runner
+chown -R actions-runner /var/actions-runner/
+sudo usermod -a -G sudo actions-runner
+sudo chsh actions-runner -s /bin/bash
+su - actions-runner
+cd /var/actions-runner
+curl -O -L https://github.com/actions/runner/releases/download/v2.262.1/actions-runner-linux-x64-2.262.1.tar.gz
+tar xzf ./actions-runner-linux-x64-2.262.1.tar.gz
+./config.sh --url https://github.com/MilStancheva/HeroesCup --token xxx
+./run.sh
+
