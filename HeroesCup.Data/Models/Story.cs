f58e5d4e09f33f4542b3c60462ee8baad1d4b41f
Milena Stancheva <milena.stancheva@miracl.com>
2020-05-21 16:03:35 +0300

    Fix story model

--- HeroesCup.Data/Models/Story.cs
+++ HeroesCup.Data/Models/Story.cs
@@ -7,8 +7,6 @@ namespace HeroesCup.Data.Models
     {
         public Guid Id { get; set; }
 
-        public string Title { get; set; }
-
         public string Content { get; set; }
 
         public Guid MissionId { get; set; }
