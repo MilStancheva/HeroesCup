9c97d8a66083ededfaea5a4cf0a1b7526c84e961
Andrey <adautev@gmail.com>
2020-05-22 21:39:55 +0300

    Removed libs code

--- HeroesCup.Libs/piranha.core/core/Piranha.Manager/assets/src/js/components/fields/text-field.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <textarea class="form-control" rows="4" :placeholder="meta.placeholder" v-model="model.value"></textarea>
-</template>
-
-<script>
-export default {
-    props: ["uid", "model", "meta"]
-}
-</script>
