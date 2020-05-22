7baed06598d475916cea72793a0447e13c4bc545
Milena Stancheva <MilStancheva@users.noreply.github.com>
2020-05-22 11:31:27 +0300

    Init Clubs (#38)
    * Add heroes in progress
    
    * Add heroes policies
    
    * Add heroes views in progress
    
    * Add clubs in progress
    
    * Add hero in progress
    
    * Add hero is coordinator
    
    * Add owner id to entities
    
    * Fix user access
    
    * Fix save result checks
    
    * Update README.md
    
    * Update README.md
    
    * Add identity seed
    
    * Update ClubsModule.csproj
    
    * Fix club service
    
    * Add club in progress
    
    * Add clubs and heroes in progress
    
    * Add club coordinator from club edit
    
    * Add club logo in progress
    
    * Add club logo image upload
    
    * Add club logo image validation
    
    * Fix custom attributes error messages
    
    * Add club delete
    
    * Add hero delete
    
    * Add button add hero in edit club view
    
    * Fix club list view
    
    * Update Edit.cshtml
    
    * Automated dotnet-format update
    
    Co-authored-by: MilStancheva <10571509+MilStancheva@users.noreply.github.com>
    
    * Update HeroesCup.sln
    
    * Add clubimage and missionimage
    
    * Fix images service
    
    * Automated dotnet-format update
    
    Co-authored-by: MilStancheva <10571509+MilStancheva@users.noreply.github.com>
    
    * Fix changed default view folder to ClubsModule
    
    Co-authored-by: Milena Stancheva <milena.stancheva@miracl.com>
    Co-authored-by: heroescup-ci[bot] <heroescup-ci[bot]@users.noreply.github.com>
    Co-authored-by: MilStancheva <10571509+MilStancheva@users.noreply.github.com>

---
+++ HeroesCup.Data/Migrations/20200514064500_UpdateModelsWithOwnerId.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HeroesCup.Data.Migrations
+{
+    public partial class UpdateModelsWithOwnerId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "OwnerId",
+                table: "Missions",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Heroes",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Clubs",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OwnerId",
+                table: "Clubs",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Missions");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Clubs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Heroes",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Clubs",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ HeroesCup.Modules/ClubsModule/Models/ClubEditModel.cs
@@ -0,0 +1,31 @@
+﻿using ClubsModule.Attributes;
+using HeroesCup.Data.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+
+namespace ClubsModule.Models
+{
+    public class ClubEditModel
+    {
+        public Club Club { get; set; }
+
+        public Hero Coordinator { get; set; }
+
+        public Guid CoordinatorId { get; set; }
+
+        [MaxSizeFile(2 * 1024 * 1024, ErrorMessage = "Maximum allowed file size is {0} bytes.")]
+        [AllowedExtensions(new string[] { ".jpg", ".png" }, ErrorMessage = "Allowed file exensions: {0}")]
+        public IFormFile UploadedLogo { get; set; }
+
+        public string LogoSrc { get; set; }
+
+        public IEnumerable<Hero> Heroes { get; set; }
+
+        public IEnumerable<Guid> HeroesIds { get; set; }
+
+        public IEnumerable<Mission> Missions { get; set; }
+
+        public IEnumerable<Guid> MissionsIds { get; set; }
+    }
+}
