2d4cd09cd26af8f7332dd6ddd5576b94b3530a17
Andrey <adautev@gmail.com>
2020-05-26 13:52:15 +0300

    Fixed syntax

--- HeroesCup.Web/Startup.cs
+++ HeroesCup.Web/Startup.cs
@@ -51,7 +51,7 @@ namespace HeroesCup
                 opt.UseMySql(connectionString));
 
             // Service setup
-            services.AddPiranha(options =>{
+            services.AddPiranha(options =>   {
                 options.UseFileStorage();
                 options.UseImageSharp();
                 options.UseManager();
