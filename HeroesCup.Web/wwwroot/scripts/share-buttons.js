320bd4d70d79f28b36e3fcbc73d8de3ce40ff442
Milena Stancheva <milena.stancheva@miracl.com>
2020-06-05 10:56:01 +0300

    Add twitter tweet button

--- HeroesCup.Web/wwwroot/scripts/share-buttons.js
+++ HeroesCup.Web/wwwroot/scripts/share-buttons.js
@@ -27,5 +27,26 @@ $(function () {
             win.opener = null;
             return false;
         });
+
+        loadTwitterSdk();
     });
+
+    function loadTwitterSdk() {
+        window.twttr = (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0],
+                t = window.twttr || {};
+            if (d.getElementById(id)) return t;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "https://platform.twitter.com/widgets.js";
+            fjs.parentNode.insertBefore(js, fjs);
+
+            t._e = [];
+            t.ready = function (f) {
+                t._e.push(f);
+            };
+
+            return t;
+        }(document, "script", "twitter-wjs"));
+    }
 });
