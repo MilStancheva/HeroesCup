40beff5c3c6c9bc5f36ddbfb3429544d3d9ec43d
Milena Stancheva <milena.stancheva@miracl.com>
2020-06-08 14:43:28 +0300

    Add share-buttons sticky position

--- HeroesCup.Web/wwwroot/scripts/navbar.js
+++ HeroesCup.Web/wwwroot/scripts/navbar.js
@@ -2,5 +2,7 @@ $(function () {
     $(document).scroll(function () {
         var $nav = $(".fixed-top");
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        var $shareButtons = $(".share-buttons");
+        $shareButtons.toggleClass('sticky', $(this).scrollTop() > $nav.height());
     });
 });
