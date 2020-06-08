0b69ec2de733aceb26a2475cdc67ec65a28d17fd
Milena Stancheva <milena.stancheva@miracl.com>
2020-06-08 15:45:55 +0300

    Add custom about page in progress

---
+++ HeroesCup.Web/Models/AboutPage.cs
@@ -0,0 +1,12 @@
+﻿using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace HeroesCup.Web.Models
+{
+    [PageType(Title = "About page")]
+    [PageTypeRoute(Title = "About", Route = "/about")]
+    public class AboutPage: Page<AboutPage>
+    {
+
+    }
+}
