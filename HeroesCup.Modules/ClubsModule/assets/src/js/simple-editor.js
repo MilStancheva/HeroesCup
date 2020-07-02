ea393ba8edc996d0151e5641da4af3aa0d28ded7
Milena Stancheva <milena.stancheva@miracl.com>
2020-07-02 11:10:47 +0300

    Update simple-editor.js

--- HeroesCup.Modules/ClubsModule/assets/src/js/simple-editor.js
+++ HeroesCup.Modules/ClubsModule/assets/src/js/simple-editor.js
@@ -1,6 +1,5 @@
 $(function () {
     var clubsSelector = 'textarea#editor';
-    var quickbarsSelectionToolbar = 'bold italic | quicklink h2 h3 blockquote quickimage quicktable';
     var height = 600;
     var heroesCupContentCss = [
         "//fonts.googleapis.com/css?family=Montserrat:300,400,500",
@@ -13,21 +12,15 @@ $(function () {
     tinymce.init({
         selector: clubsSelector,
         menubar: menubar,
-        statusbar: statusbar,
+        statusbar: false,
         branding: branding,
         inline: false,
         plugins: simple_plugins,
         width: width,
         autoresize_min_height: autoresizeMinHeight,
         toolbar: simple_toolbar,
-        block_formats: blockFormats,
-        formats: formats,
-        style_formats: styleFormats,
         content_css: heroesCupContentCss,
-        font_formats: fontFormats,
-        fontsize_formats: fontsizeFormats,
         height: height,
-        quickbars_selection_toolbar: quickbarsSelectionToolbar,
         import_css_append: importCssAppend,
     });
 })
