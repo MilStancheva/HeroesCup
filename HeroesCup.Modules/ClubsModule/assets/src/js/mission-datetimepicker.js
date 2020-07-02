41a520c49c45876e545263c793e5b6311f5f26c2
Milena Stancheva <milena.stancheva@miracl.com>
2020-07-02 10:03:35 +0300

    Fix mission dates

--- HeroesCup.Modules/ClubsModule/assets/src/js/mission-datetimepicker.js
+++ HeroesCup.Modules/ClubsModule/assets/src/js/mission-datetimepicker.js
@@ -1,9 +1,22 @@
 ﻿$(function () {
-    var  config;
-    config = {
-        format: 'dd mm yyyy'
+    var start_date_config, end_date_config;
+    var today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+
+    start_date_config = {
+        format: 'dd mm yyyy',
+        minDate: today,
+        maxDate: function () {
+            return $('#end_date').val();
+        }
     };
 
-    $("#start_date").datepicker(config);
-    $("#end_date").datepicker(config);
+    end_date_config = {
+        format: 'dd mm yyyy',
+        minDate: function () {
+            return $('#start_date').val();
+        }
+    };
+
+    $("#start_date").datepicker(start_date_config);
+    $("#end_date").datepicker(end_date_config);
 });
