bb41305d2747eece002b50b5b2a50d7ca44f38a0
Milena Stancheva <MilStancheva@users.noreply.github.com>
2020-07-20 09:58:48 +0300

    Fixes (#81)
    * Fix story images
    
    * Fix styles, favicon, mobile navbar fixed, about page
    
    * Fix buttom participate
    
    Co-authored-by: Milena Stancheva <milena.stancheva@miracl.com>

--- HeroesCup.Web/wwwroot/scripts/navbar.js
+++ HeroesCup.Web/wwwroot/scripts/navbar.js
@@ -1,15 +1,8 @@
 $(function () {
     $(document).scroll(function () {
         var $nav = $(".fixed-top");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-        var $shareButtons = $(".share-buttons");
-        $shareButtons.toggleClass('sticky', $(this).scrollTop() > $nav.height());
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());        
     });
-
-    //$(document).on("slide", function () {
-    //    var $nav = $(".fixed-top");
-    //    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    //});
 
     $("#btn-nav-toggle").click(function () {
        
