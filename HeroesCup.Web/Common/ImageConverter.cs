1fca10a877a854d8f5525580fdbad53c4a79853d
Milena Stancheva <milena.stancheva@miracl.com>
2020-08-23 09:44:27 +0300

    Fix mission facebook share button

---
+++ HeroesCup.Web/Common/ImageConverter.cs
@@ -0,0 +1,14 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace HeroesCup.Web.Common
+{
+    public static class ImageConverter
+    {
+        public static Image ConvertByteArrayToImage(this byte[] byteArray)
+        {
+            var image = Image.LoadPixelData<Rgba32>(byteArray, 10, 10);            
+            return image;
+        }
+    }
+}
