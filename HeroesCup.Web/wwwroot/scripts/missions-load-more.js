42b9b197778ee2f2ab61dee3c46c596651c129fa
Milena Stancheva <milena.stancheva@miracl.com>
2020-08-29 14:38:04 +0300

    Update missions-load-more.js

--- HeroesCup.Web/wwwroot/scripts/missions-load-more.js
+++ HeroesCup.Web/wwwroot/scripts/missions-load-more.js
@@ -1,22 +1,19 @@
 ﻿$(function () {
     $(document).ready(function () {
         $("#load-missions").click(function () {
-            $("#missions-with-banner-partial").load("missions/load-missions", { loadRequest: true });
-            $(window).ready(function () {
-                $("#load-missions-container").hide();           
+            $("#missions-with-banner-partial").load("missions/load-missions", { loadRequest: true }, function () {
+                $("#load-missions-container").hide(); 
             });
         });
 
         $("#load-missionideas").click(function () {
-            $("#missionideas-partial").load("missions/load-missionideas", { loadRequest: true });
-            $(window).ready(function () {
+            $("#missionideas-partial").load("missions/load-missionideas", { loadRequest: true }, function () {
                 $("#load-missionideas-container").hide();
-            });          
+            });         
         });
 
         $("#load-stories").click(function () {
-            $("#stories-partial").load("missions/load-stories", { loadRequest: true });
-            $(window).ready(function () {
+            $("#stories-partial").load("missions/load-stories", { loadRequest: true }, function () {
                 $("#load-stories-container").hide();
             });            
         });
